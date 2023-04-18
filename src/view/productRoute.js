6438002e8961070084371a8410a43139830bdcf0
Suraj Santosh Pawar <surajpawar913@gmail.com>
2023-04-18 17:17:50 +0530

    Add files via upload

---
+++ src/view/productRoute.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const app=express.Router()
+const {postProduct, deleteProduct, patchProduct, getProduct,getSingleProduct} = require("../controller/productController")
+
+app.post("/post",postProduct)
+app.delete("/:id",deleteProduct)
+app.patch("/:id",patchProduct)
+app.get("/get",getProduct)
+app.get("/:id",getSingleProduct)
+
+
+
+
+module.exports =app
+
+
